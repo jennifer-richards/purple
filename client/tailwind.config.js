3d937a81fec067e50a7ffce4116055c99c5cab69
Matthew Holloway <matthew@staff.ietf.org>
2025-06-27 13:38:50 +1200

    fix: tailwind globbing wasn't looking at enough files (#370)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@ import formsPlugin from '@tailwindcss/forms'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./layouts/*.{html,ts,vue}",
+    "./pages/*.{html,ts,vue}",
+    "./utilities/*.{html,ts,vue}",
+    "./components/*.{html,ts,vue}",
+  ],
   darkMode: 'class',
   plugins: [
     formsPlugin
