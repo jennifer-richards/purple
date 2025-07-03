abc41366fee1e36861cd7c2a4b9b978c631b13fa
Jennifer Richards <jennifer@staff.ietf.org>
2025-07-03 12:05:46 -0300

    chore: reconcile migration numbering

--- rpc/migrations/0018_alter_historicalrfctobe_options_and_more.py
+++ rpc/migrations/0018_alter_historicalrfctobe_options_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies = [
         ("datatracker", "0004_historicaldatatrackerperson"),
-        ("rpc", "0016_populate_doc_relationship_names"),
+        ("rpc", "0017_blankable_rpcrelateddocument_targets"),
     ]
 
     operations = [
