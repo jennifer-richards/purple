e4c154b8ca155ed3c7129aa54500777ed3fa13ec
Jennifer Richards <jennifer@staff.ietf.org>
2025-09-17 14:51:34 -0300

    ci: trigger publish-wiki; use node 22; cleanup (#491)

--- docs/docgen.config.js
+++ docs/docgen.config.js
@@ -1,17 +1,6 @@
-// const path = require('path')
-
+// Documentation of options at https://github.com/vue-styleguidist/vue-styleguidist/tree/dev/packages/vue-docgen-cli
 module.exports = {
   componentsRoot: '../client/app/components', // the folder where CLI will start searching for components.
   components: '**/[A-Z]*.vue', // the glob to define what files should be documented as components (relative to componentRoot)
   outDir: 'output' // folder to save components docs in (relative to the current working directry)
-  // templates: {
-  //   // global component template wrapping all others see #templates
-  //   component: require('templates/component'),
-  //   events: require('templates/events'),
-  //   methods: require('templates/methods'),
-  //   props: require('templates/props'),
-  //   slots: require('templates/slots'),
-  //   // static template to display as a tag if component is functional
-  //   functionalTag: '**functional**'
-  // }
 }
