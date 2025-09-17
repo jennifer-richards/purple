c542e6eb2e6205d055cc87f6624b01514c3fbc1e
Jennifer Richards <jennifer@staff.ietf.org>
2025-09-17 11:32:39 -0300

    ci: fix vue-docgen config/usage (#490)

--- docs/docgen.config.js
+++ docs/docgen.config.js
@@ -1,7 +1,7 @@
 // const path = require('path')
 
 module.exports = {
-  componentsRoot: '../client/components', // the folder where CLI will start searching for components.
+  componentsRoot: '../client/app/components', // the folder where CLI will start searching for components.
   components: '**/[A-Z]*.vue', // the glob to define what files should be documented as components (relative to componentRoot)
   outDir: 'output' // folder to save components docs in (relative to the current working directry)
   // templates: {
