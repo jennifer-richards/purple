a7400149164644f135a31da80cf94aca4599ec27
Rudi Matz <rudi@staff.ietf.org>
2025-09-29 10:09:38 -0400

    fix: restore previously reverted change of icon (#506)

--- client/app/components/RpcTextarea.vue
+++ client/app/components/RpcTextarea.vue
@@ -2,11 +2,7 @@
 <template>
   <div class="flex items-start space-x-4">
     <div class="flex-shrink-0">
-      <img
-        class="inline-block h-10 w-10 rounded-full"
-        src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        alt=""
-      />
+      <Icon name="mdi:comment-text-outline" class="mr-1" />
     </div>
     <div class="min-w-0 flex-1">
       <form action="#" class="relative" @submit.prevent="handleSubmit">
