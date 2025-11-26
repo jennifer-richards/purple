a3d48a59865cef5bf1d44add76e36aa2f1fb0cea
Jennifer Richards <jennifer@staff.ietf.org>
2025-11-26 11:37:33 -0400

    chore: delay celery start until migrations done (#651)

--- dev/build/celery-start.sh
+++ dev/build/celery-start.sh
@@ -3,8 +3,27 @@
 # Startup script for the celery container
 #
 
-# specify celery location
+# specify binary locations
 CELERY=/usr/local/bin/celery
+MANAGE_PY=./manage.py
+
+migrations_applied_for () {
+    local DATABASE=${1:-default}
+    $MANAGE_PY migrate --check --database "$DATABASE"
+}
+
+migrations_all_applied () {
+    migrations_applied_for default
+}
+
+if ! migrations_all_applied; then
+    echo "Unapplied migrations found, waiting to start..."
+    sleep 5
+    while ! migrations_all_applied ; do
+        echo "... still waiting for migrations..."
+        sleep 5
+    done
+fi
 
 echo "Starting Purple celery container..."
 
