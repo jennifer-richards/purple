e2ee56dd1d3e073b1ae467cdb41dc71767eec82b
Kesara Rathnayake <kesara@fq.nz>
2025-11-29 08:26:36 +1300

    ci: Run django tests (#641)

--- purple/settings/development.py
+++ purple/settings/development.py
@@ -50,7 +50,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": "db",
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
         "PORT": 5432,
     }
 }
