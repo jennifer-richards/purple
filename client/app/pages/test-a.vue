d080c704baa8f8200a5309cfafbc0e9b3422e40a
Matthew Holloway <matthew@staff.ietf.org>
2026-03-12 17:36:35 +1300

    chore: test auth overhead routes (#973)

---
+++ client/app/pages/test-a.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Test route (a) please ignore</h1>
+    <p>This authenticated route is used to test the overhead of auth. It doesn't make any other API calls.</p>
+    <p><Anchor href="/test-b" :class="ANCHOR_STYLE">go to test-b</Anchor></p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ANCHOR_STYLE } from '../utils/html'
+useHead({
+  title: 'Test route (a)'
+})
+</script>
---
+++ client/app/pages/test-b.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Test route (b) please ignore</h1>
+    <p>This authenticated route is used to test the overhead of auth. It doesn't make any other API calls.</p>
+    <p><Anchor href="/test-a" :class="ANCHOR_STYLE">go to test-a</Anchor></p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ANCHOR_STYLE } from '../utils/html'
+useHead({
+  title: 'Test route (b)'
+})
+</script>
