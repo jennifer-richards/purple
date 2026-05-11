8899913570ccf8a584514710de1ace2ab390a888
Rudi Matz <rudi@staff.ietf.org>
2026-05-11 09:29:19 -0400

    feat: show page count on queue page (#1211)

--- client/app/pages/queue/queue.vue
+++ client/app/pages/queue/queue.vue
@@ -384,6 +384,14 @@ const columns = [
     }
   ),
   columnHelper.accessor(
+    'pages',
+    {
+      header: 'Pages',
+      cell: data => data.getValue() ?? '-',
+      sortingFn: 'alphanumeric',
+    }
+  ),
+  columnHelper.accessor(
     'cluster',
     {
       header: 'Cluster',
