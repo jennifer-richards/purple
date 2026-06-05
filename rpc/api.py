9d7ba0c21c75cb2b1e4343a3a6b963cfa4b4ad16
Rudi Matz <rudi@staff.ietf.org>
2026-06-05 12:29:53 -0400

    fix: allow import draft that was previously withdrawn (#1338)
    * fix: allow import draft that was previously withdrawn
    
    * ruff

--- rpc/api.py
+++ rpc/api.py
@@ -430,10 +430,12 @@ def submissions(request, *, rpcapi: rpcapi_client.PurpleApi):
     # Get submissions list from Datatracker
     with datatracker_api():
         submitted = rpcapi.submitted_to_rpc()
-    # Filter out I-Ds that already have an RfcToBe
-    already_in_queue = RfcToBe.objects.filter(
-        draft__datatracker_id__in=[s.id for s in submitted]
-    ).values_list("draft__datatracker_id", flat=True)
+    # Filter out I-Ds that already have an active (non-withdrawn) RfcToBe
+    already_in_queue = (
+        RfcToBe.objects.filter(draft__datatracker_id__in=[s.id for s in submitted])
+        .exclude(disposition__slug="withdrawn")
+        .values_list("draft__datatracker_id", flat=True)
+    )
     submitted = [s for s in submitted if s.id not in already_in_queue]
     return Response(SubmissionListItemSerializer(submitted, many=True).data)
 
